2eb9c76c1c0ccd77d80ff884d7f3a6067d64bc3e
IDGS-805-23002597 <ijaquelinegagon@gmail.com>
2026-02-14 15:31:13 -0600

    config 13/02/2026

--- .env/Scripts/activate.bat
+++ .env/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\IDGS805\FlaskBDIDGS805\.env
+set VIRTUAL_ENV=C:\Users\vicky\FlaskBDIDGS805\.env
 
 if not defined PROMPT set PROMPT=$P$G
 
